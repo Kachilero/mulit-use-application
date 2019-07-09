bdfe6e71a9901f0fdc207332dd29966e87a11a1b
Alejandro <alejcerro@gmail.com>
2019-07-08 20:08:08 -0500

    Initial wiring seems to work

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -1,9 +1,27 @@
-import * as React from 'react';
-import { Component } from 'react';
-import OgHome from '../components/OgHome';
+import { bindActionCreators, Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import MainWindow from '../components/mainWindow';
+import SidebarActions from '../actions/sideDrawer';
 
-export default class HomePage extends Component {
-  render() {
-    return <OgHome />;
-  }
+export interface MainWindowState {
+  flipSidebarState: () => void;
+  addActiveClass: () => void;
+  headline: string;
 }
+
+function mapStateToProps(state: MainWindowState) {
+  return {
+    flipSidebarState: state.flipSidebarState,
+    addActiveClass: state.addActiveClass,
+    headline: state.headline
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+  return bindActionCreators(SidebarActions, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MainWindow);
