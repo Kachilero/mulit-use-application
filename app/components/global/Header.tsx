eb6b5012568c832349a2df91f160d18372ce9de6
Alejandro <alejcerro@gmail.com>
2019-07-13 17:59:35 -0500

    Added some links to the Header

--- app/components/global/Header.tsx
+++ app/components/global/Header.tsx
@@ -4,7 +4,11 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { Navbar } from 'react-bootstrap';
+import NavbarToggle from 'react-bootstrap/NavbarToggle';
+import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
+import NavLink from 'react-bootstrap/NavLink';
 
+const routes = require('../../constants/routes.json');
 const userDefaults = require('../../constants/userDefaults.json');
 
 type Props = {};
@@ -19,9 +23,12 @@ export default class Header extends Component<Props> {
         variant={userDefaults.default.Navbar.variant}
         fixed={userDefaults.default.Navbar.fixed}
       >
-        <div className="container-fluid">
-          <Navbar.Brand>Multi Use App</Navbar.Brand>
-        </div>
+        <Navbar.Brand>Multi Use App</Navbar.Brand>
+        <NavbarToggle aria-controls="main-navbar-nav" />
+        <NavbarCollapse id="main-navbar-nav">
+          <NavLink href={`#${routes.HOME}`}>Home</NavLink>
+          <NavLink href={`#${routes.COUNTER}`}>Counter</NavLink>
+        </NavbarCollapse>
       </Navbar>
     );
   }
